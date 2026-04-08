4ff0b6fc6bdeb11eecd06b0b15f765aef6f44689
vedangk1234 <vedangk2912@gmail.com>
2026-04-08 14:42:20 +0530

    fix: update nakama server URL to railway domain

--- frontend/src/nakama.js
+++ frontend/src/nakama.js
@@ -1,6 +1,10 @@
+import { Client } from "@heroiclabs/nakama-js";
+
 const client = new Client(
   "defaultkey",
-  "nakama-tictactoe-production-b830.up.railway.app",
+  "nakama-tictactoe-production-3360.up.railway.app",
   "443",
   true
 );
+
+export default client;
